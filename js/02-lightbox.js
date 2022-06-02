a664ce21e9c1b053693b3686b1643966f175d88e
Andrey Pakhomov <pahomeathome@gmail.com>
2022-06-02 13:31:07 +0300

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(`.gallery`);
+
+function createGallery(gallery) {
+    
+    const array = [];
+    
+    gallery.map(({ preview, original, description }) => {
+        
+    const link = document.createElement(`a`);
+    link.classList.add(`gallery__item`);
+    link.href = original;
+
+    const image = document.createElement(`img`);
+    image.classList.add(`gallery__image`);
+    image.src = preview;
+    image.alt = description
+        link.append(image);
+
+        array.push(link)
+    })
+
+    return array;
+}
+    
+const galleryMarkup = createGallery(galleryItems);
+
+gallery.append(...galleryMarkup);
+
+const lightBox = new SimpleLightbox(`.gallery a`, { captionsData: `alt`, captionDelay: 250 });
+lightBox.on('show.simplelightbox')
